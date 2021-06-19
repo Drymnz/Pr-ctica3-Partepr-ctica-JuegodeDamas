e1412421ceede3f4eb33a69d7a17b69a3b1d3609
Benjamín de Jesús Pérez Aguilar <benjaminperez201831504@cunoc.edu.gt>
2021-06-18 21:30:12 -0600

    fue terminado la ficha

--- src/JuegoDamas/Casilla.java
+++ src/JuegoDamas/Casilla.java
@@ -1,5 +1,104 @@
 package src.JuegoDamas;
 
 public class Casilla {
-    
+    private Ficha ficha;
+    private String color;
+    private int ancho;
+    private int alto;
+
+    // contructor
+    public Casilla(Ficha ficha, String color, int ancho, int alto) {
+        this.ficha = ficha;
+        this.color = color;
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    public Casilla(String color) {
+        this(null, color, 3, 3);
+    }
+    // fin de constructor
+    // dibujar espacio
+    public void dibujar(int linea){
+        if (alto >= linea) {
+             // dibujar ariva y dibujar abajo
+            if (linea != (centroAlto())) {
+                dibujarLinea();
+            }else{
+                // dibujar centro
+                for (int i = 0; i < ancho; i++) {
+                    System.out.print(ficha.getCaracter());
+                }
+            } 
+        }
+    }
+
+    private void dibujarLinea() {
+        for (int i = 0; i < ancho; i++) {
+            if (i == centroAncho() && ficha != null) {
+                System.out.print(ficha.getCaracter());
+            }
+            System.out.print(color);
+        }
+    }
+
+    private int centroAlto() {
+        boolean parImpar = (alto % 2) == 0;
+        if (parImpar) {
+            return alto / 2;
+        } else {
+            return (alto / 2) + 1;
+        }
+    }
+
+    private int centroAncho() {
+        boolean parImpar = (ancho % 2) == 0;
+        if (parImpar) {
+            return ancho / 2;
+        } else {
+            return (ancho / 2) + 1;
+        }
+    }
+
+    // fin dibujar espacio
+    // tengo ficha en mi casilla
+    public boolean getTengoFicha() {
+        return ficha != null;
+    }
+
+    // get
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getAltura() {
+        return alto;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    // fin de get
+    // set
+    public void setFicha(Ficha ficha) {
+        this.ficha = ficha;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setAltura(int alto) {
+        this.alto = alto;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+    // fin set
 }
--- src/herramientas/Start.java
+++ src/herramientas/Start.java
@@ -1,18 +1,22 @@
 package src.herramientas;
+
 public class Start {
     public static void main(String[] args) {
         Menu ejecutar = new Menu();
     }
-    public static void colores(){
-        String[] colores  = {"\u001B[30m","\u001B[31m","\u001B[32m","\u001B[33m","\u001B[34m","\u001B[35m", "\u001B[36m","\u001B[37m"};
-        String[] reslatar = {"\u001B[40m","\u001B[41m","\u001B[42m","\u001B[43m","\u001B[44m","\u001B[45m","\u001B[46m","\u001B[47m"};
+
+    public static void colores() {
+        String[] colores = { "\u001B[30m", "\u001B[31m", "\u001B[32m", "\u001B[33m", "\u001B[34m", "\u001B[35m",
+                "\u001B[36m", "\u001B[37m" };
+        String[] reslatar = { "\u001B[40m", "\u001B[41m", "\u001B[42m", "\u001B[43m", "\u001B[44m", "\u001B[45m",
+                "\u001B[46m", "\u001B[47m" };
         String reset = "\u001B[0m";
         for (int i = 0; i < colores.length; i++) {
-            System.out.println(colores[i]+"puevas de colores"+reset);
+            System.out.println(colores[i] + "puevas de colores" + reset);
         }
         System.out.println("!");
         for (int i = 0; i < reslatar.length; i++) {
-            System.out.println(reslatar[i]+"puevas de colores"+reset);
+            System.out.println(reslatar[i] + "puevas de colores" + reset);
         }
     }
 }
