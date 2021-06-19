b18cf9cc0d15feb456419c236d365ccdc92e74f5
Benjamín de Jesús Pérez Aguilar <benjaminperez201831504@cunoc.edu.gt>
2021-06-18 20:51:52 -0600

    termine con el codio de ficha

--- src/JuegoDamas/Ficha.java
+++ src/JuegoDamas/Ficha.java
@@ -1,5 +1,29 @@
 package src.JuegoDamas;
 
 public class Ficha {
-    
+    private String caracter;
+    private int id;
+    // constructor
+    public Ficha(String caracter, int id){
+        this.caracter = caracter;
+        this.id = id;
+    }
+    public Ficha(int id){
+        this("#", id);
+    }
+    //fin constructor
+    // get y set
+    public int getId(){
+        return id;
+    }
+    public String getCaracter(){
+        return caracter;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setCaracter(String caracter){
+        this.caracter = caracter;
+    }
+    // fin get y set
 }
