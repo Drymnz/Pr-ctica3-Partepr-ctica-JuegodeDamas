763badefe0e1003cffa59d1690684b02a32f3f49
Benjamín de Jesús Pérez Aguilar <benjaminperez201831504@cunoc.edu.gt>
2021-06-19 07:59:58 -0600

    fue terminado el dibujo de la tabla con las fichas bien colocadas

--- src/JuegoDamas/Tabla.java
+++ src/JuegoDamas/Tabla.java
@@ -3,6 +3,7 @@ package src.JuegoDamas;
 public class Tabla {
     private final int ancho;
     private final int alto;
+    private int espaciosBlanco = 0;
     private Ficha[] fichasJugadorUno;
     private Ficha[] fichasJugadorDos;
     private Casilla[][] tabla;
@@ -10,9 +11,10 @@ public class Tabla {
     private String colorDos;
 
     // constructor
-    public Tabla(int ancho, int alto,String colorUno, String colorDos) {
+    public Tabla(int ancho, int alto,int espaciosBlanco, String colorUno, String colorDos) {
         this.ancho = ancho;
         this.alto = alto;
+        this.espaciosBlanco = espaciosBlanco;
         this.colorUno = colorUno;
         this.colorDos = colorDos;
         fichasJugadorUno = new Ficha[ancho];
@@ -20,58 +22,98 @@ public class Tabla {
         tabla = new Casilla[ancho][alto];
         crearFichas();
         crearCasilla();
+        // coloca fichas
+        llenarTablaFichas(0, fichasJugadorUno);
+        llenarTablaFichas(alto - 2, fichasJugadorDos);
     }
+
     public Tabla(int ancho, int alto) {
-        this(ancho, alto, "\u001B[40m", "\u001B[47m");
+        this(ancho, alto,0,"\u001B[40m", "\u001B[47m");
     }
-    
+
+    // fin contructor
+    // crear fichas y casillas, y colocarlas en sus posicion inicial
     private void crearFichas() {
         for (int i = 0; i < ancho; i++) {
             fichasJugadorUno[i] = new Ficha(i);
             fichasJugadorDos[i] = new Ficha(i);
         }
     }
-
-    private void crearCasilla(){
+    //aqui crea las casiilas y le su color clasico
+    private void crearCasilla() {
         String colorOne = "";
         String colorTwo = "";
-        for (int i = 0; i < fichasJugadorDos.length; i++) {
-            colorOne = (((i%2)==0)  )? colorUno : colorDos;
-            colorTwo = (((i%2)==0)  )? colorDos : colorUno;
-            for (int j = 0; j < fichasJugadorDos.length; j++) {
-                String color = (((j%2)==0)  )? colorOne : colorTwo;
+        for (int i = 0; i < alto; i++) {
+            colorOne = (((i % 2) == 0)) ? colorUno : colorDos;
+            colorTwo = (((i % 2) == 0)) ? colorDos : colorUno;
+            for (int j = 0; j < ancho; j++) {
+                String color = (((j % 2) == 0)) ? colorOne : colorTwo;
                 tabla[i][j] = new Casilla(color);
             }
         }
     }
 
-    // fin contructor
-    // dibujar tabla
-    public void dibujar( int altoEspacioCasilla) {
-        for (int i = 0; i < alto; i++) {
-            for (int j = 1; j < (altoEspacioCasilla+1); j++) {
-                for (int j2 = 0; j2 < ancho; j2++) {
-                    tabla[i][j2].dibujar(j);
-                }
-                System.out.println("");
+    private void llenarTablaFichas(int filaUno, Ficha[] ordenar) {
+        for (int i = filaUno; i < filaUno + 2; i++) {
+            int contador = (i == filaUno) ? 0 : ((ancho / 2));
+            for (int j = (contador == 0) ? 0 : 1; j < ancho; j += 2) {
+                tabla[i][j].setFicha(ordenar[contador]);
+                contador++;
             }
         }
     }
-    public void dibujar(){
-        llenarTablaFichas(0,fichasJugadorUno);
-        llenarTablaFichas(alto-2,fichasJugadorDos);
-        System.out.println("012345678901234567890123456789");
-        this.dibujar(3);
+
+    // fin crear fichas y casillas, y colocarlas en sus posicion inicial
+    // dibujar tabla
+
+    public void dibujar(int filas) {
+        guiasTablaSuperior();
+        for (int i = 0; i < filas; i++) {
+            for (int j = 1; j < (tabla[0][0].getAltura() + 1); j++) {
+                espacios();
+                guiaTablaLateral((j-1),(i+1),false);
+                for (int j2 = 0; j2 < ancho; j2++) {
+                    tabla[i][j2].dibujar(j);
+                }
+                guiaTablaLateral((j-1),(i+1),true);
+            }
+        }
+        guiasTablaSuperior();
     }
-    private void llenarTablaFichas(int filaUno,Ficha[] ordenar){
-        for (int i = filaUno; i < filaUno+2; i++) {
-            int contador = (i==0)? 0: ((ancho / 2));
-            for (int j = (contador == 0)? 0: 1; j < ancho; j+=2) {
-                tabla[i][j].setFicha(ordenar[contador]);
-                contador ++;
-            } 
+
+    public void dibujar() {
+        this.dibujar(alto);
+    }
+
+    private void espacios(){
+        for (int i = 0; i < espaciosBlanco; i++) {
+            System.out.print(" ");
         }
     }
-   
+
+    private void guiaTablaLateral(int fila,int contador,boolean saltoLinea) {
+        if (fila == (tabla[0][0].getAltura() / 2)) {
+            System.out.print((colorUno+contador+ "\u001B[0m"));
+        } else {
+            System.out.print(colorDos+"*"+ "\u001B[0m");
+        }
+        String imprimir = (saltoLinea)? ("\n") : "";
+        System.out.print(imprimir);
+    }
+
+    private void guiasTablaSuperior() {
+        espacios();
+        System.out.print(colorDos+"*"+ "\u001B[0m");
+        for (int j = 0; j < ancho; j++) {
+            for (int i = 0; i < tabla[0][0].getAncho(); i++) {
+                if (i == (tabla[0][0].getAncho() / 2)) {
+                    System.out.print(colorUno+(j + 1)+ "\u001B[0m");
+                } else {
+                    System.out.print(colorDos+"*"+ "\u001B[0m");
+                }
+            }
+        }
+        System.out.print(colorDos+"*"+ "\u001B[0m \n");
+    }
     // fin dibujar tabla
 }
