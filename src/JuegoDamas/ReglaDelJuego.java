898871c860642e723b9325cdca4918217d02fc7b
Benjamín de Jesús Pérez Aguilar <benjaminperez201831504@cunoc.edu.gt>
2021-06-20 17:40:07 -0600

    esta bien el movimiento simple

--- src/JuegoDamas/ReglaDelJuego.java
+++ src/JuegoDamas/ReglaDelJuego.java
@@ -2,7 +2,7 @@ package src.JuegoDamas;
 
 public class ReglaDelJuego {
     private int puntoAFavor;
-    private Tabla tabla;
+    private Casilla[][] tabla;
     private Ficha ficha;
     private boolean valido;
     private int posicionInicialX;
@@ -10,67 +10,68 @@ public class ReglaDelJuego {
     private int posicionFinalX;
     private int posicionFinalY;
     private int numeroJugador;
-    private final int[][] matrizGuia;
 
     // constructor
-    public ReglaDelJuego(int puntoAFavor, Tabla tabla, Ficha ficha, boolean valido, int posicionInicialX,
+    public ReglaDelJuego(int puntoAFavor, Casilla[][] tabla, Ficha ficha, boolean valido, int posicionInicialX,
             int posicionInicialY, int posicionFinalX, int posicionFinalY, int numeroJugador) {
         this.puntoAFavor = puntoAFavor;
         this.tabla = tabla;
         this.ficha = ficha;
         this.valido = valido;
         this.posicionInicialX = posicionInicialX;
+        System.out.println("posicionInicialX >>" + posicionInicialX);
         this.posicionInicialY = posicionInicialY;
+        System.out.println("posicionInicialY>>>" + posicionInicialY);
         this.posicionFinalX = posicionFinalX;
+        System.out.println("posicionFinalX>>>" + posicionFinalX);
         this.posicionFinalY = posicionFinalY;
+        System.out.println("posicionFinalY>>" + posicionFinalY);
         this.numeroJugador = numeroJugador;
-        this.matrizGuia = new int[tabla.getAncho()][tabla.getAlto()];
-        matrizGuia();
         movimiento();
     }
-    public ReglaDelJuego(Tabla tabla, Ficha ficha, int posicionInicialX, int posicionInicialY, int posicionFinalX,
+
+    public ReglaDelJuego(Casilla[][] tabla, Ficha ficha, int posicionInicialX, int posicionInicialY, int posicionFinalX,
             int posicionFinalY, int numeroJugador) {
         this(0, tabla, ficha, false, posicionInicialX, posicionInicialY, posicionFinalX, posicionFinalY, numeroJugador);
     }
 
     // fin constructor
-    // matriz guia para el movimiento
-    public void matrizGuia() {
-        int contador = 1;
-        for (int x = 0; x < tabla.getAncho(); x++) {
-            for (int y = 0; y < tabla.getAlto(); y++) {
-                matrizGuia[x][y] = contador;
-                contador++;
-            }
-        }
-    }
-
-    // fin matriz guia
     // tipo de movimiento
     private void movimiento() {
-        // verificara si la direccion de la ficha esta corecto respecto al jugador, pero si esta coronado no le importara
-        if (direccionMovimiento()||(ficha.getCoronado())) {
-            boolean tipoMovimiento = (distanciaMovidaFilas()==1)? true : false;
-            if (tipoMovimiento) {
-                valido = posicionFinalNoHayFicha() ;
-            } else {
-                movimientoKill();
-            }
+        // movimiento simple
+        System.out.println("distanciaEntreFilas()");
+        if ((distanciaEntreFilas()== distanciaEntreColumnas()) && retrozederFicha()) {
+            System.out.println("solo una fila");
+                valido = siSonIgualColor() && espacioDisponible();
+        } else {
+            System.out.println("tengo fiahca >>>" + tabla[posicionFinalX][posicionFinalY].getTengoFicha());
+                valido = tabla[posicionFinalX][posicionFinalY].getTengoFicha();
         }
+    }
+    // espacio disponible
+    private boolean espacioDisponible(){
+        return !tabla[posicionFinalX][posicionFinalY].getTengoFicha();
     }
 
-    private void movimientoKill() {
-        // un kill
-        System.out.println(("kill"+"distaci"+distanciaMovidaFilas()+"fichaa"+posicionFinalNoHayFicha()));
-        if ((distanciaMovidaFilas()==2)&& posicionFinalNoHayFicha()) {
-            System.out.println("muerte a "+ matrizGuia[posicionFinalX][posicionFinalY]);
-            tabla.getTabla()[posicionFinalX][posicionFinalY].setFicha(null);
-            puntoAFavor ++;
-            valido = true;
-        }
+    // que mida la distancia entre filas para que no realize saltos
+    private boolean retrozederFicha(){
+        int resultado = (numeroJugador == 0) ? posicionFinalY - posicionInicialY : posicionInicialX - posicionFinalX;
+        return (resultado>0) || ficha.getCoronado();
     }
-    private boolean posicionFinalNoHayFicha() {
-        return !(tabla.getTabla()[posicionFinalX][posicionFinalY].getTengoFicha());
+  
+    private int distanciaEntreFilas(){
+        int resultado = (numeroJugador == 0) ? posicionFinalY - posicionInicialY : posicionInicialX - posicionFinalX;
+        return (resultado < 0)? (resultado*-1) : resultado;
+    }
+    private int distanciaEntreColumnas(){
+        int resultado = (numeroJugador == 0) ? posicionFinalX - posicionInicialX : posicionInicialX - posicionFinalX;
+        return (resultado < 0)? (resultado*-1) : resultado;
+    }
+    
+   
+    // que solo se mueve en un color de todos
+    private boolean siSonIgualColor() {
+        return ((posicionInicialX % 2) != (posicionFinalX % 2)) && ((posicionInicialY % 2) != (posicionFinalY % 2));
     }
 
     // get
@@ -83,27 +84,4 @@ public class ReglaDelJuego {
     }
 
     // fin get
-    // que los de la fichas superior se mueva asi abajo y lo contario,no se puede regresar
-    private boolean direccionMovimiento() {
-        return ((numeroJugador == 0) ? (valorIniciar() < valorFinal()) : ((valorIniciar() > valorFinal()))) && (mismoLiena());
-    }
-
-    private boolean mismoLiena() {
-        return (posicionInicialY != posicionFinalY);
-    }
-
-    // valor de matriz de guia
-    private int valorIniciar() {
-        return matrizGuia[posicionInicialX][posicionInicialY];
-    }
-
-    private int valorFinal() {
-        return matrizGuia[posicionFinalX][posicionFinalY];
-    }
-    // distancia, me sirvio para ver si se quiere mover mas de dos filas
-      private int distanciaMovidaFilas() {
-        int resultado = (numeroJugador == 0)? posicionFinalX - posicionInicialY : posicionFinalY   - posicionInicialY;
-        return ((resultado)<0)? (resultado*-1) : resultado;
-    }
-    // fin distancia
 }
